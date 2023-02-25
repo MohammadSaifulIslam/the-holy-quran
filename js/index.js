e8e978eb1a044093101481175bbf02e18de0a00a
MohammadSaifulIslam <saifmdislam231@gmail.com>
2023-02-25 15:58:52 +0600

    surah search, data find and display part done

--- js/index.js
+++ js/index.js
@@ -1,44 +1,48 @@
-const loadData = async () => {
-try{
-    const url = 'http://api.alquran.cloud/v1/surah';
-    const res = await fetch(url);
-    const data = await res.json() ;
-    showData(data.data)
-}
-catch(error){
-    console.log(error);
-}
+const loadSurah = async () => {
+    try {
+        const url = 'http://api.alquran.cloud/v1/surah';
+        const res = await fetch(url);
+        const data = await res.json();
+        showSurah(data.data)
+    }
+    catch (error) {
+        console.log(error);
+    }
 };
 
-const showData = surahs => {
+const showSurah = surahs => {
+    const surahListContainer = document.getElementById('surah-list');
     // console.log(surahs)
-    surahs.forEach( surah => {
-        // console.log(surah)
-        for(let key in surah){
-            // console.log(key)
-        }
+    surahs.forEach(surah => {
+        console.log(surah)
+        const option = document.createElement('option');
+        option.innerHTML = `
+        Surah No: ${surah.number} ${surah.englishName}
+        `;
+        option.value = `${surah.number}`
+        surahListContainer.appendChild(option)
     });
 }
 
-
-const loadAyah = async () => {
-try{
-    const url = 'https://api.alquran.cloud/v1//surah/1';
-    const res = await fetch(url);
-    const data = await res.json() ;
-    showAyah(data.data.ayahs)
-}
-catch(error){
-    console.log(error);
-}
+const loadAyah = async (surahNumber) => {
+    try {
+        const url = `https://api.alquran.cloud/v1//surah/${surahNumber}`;
+        const res = await fetch(url);
+        const data = await res.json();
+        showAyah(data.data.ayahs)
+        console.log(url)
+    }
+    catch (error) {
+        console.log(error);
+    }
 };
 
 
 
 
 const showAyah = ayahs => {
-    // console.log(surahs)
     const ayahContainer = document.getElementById('surah-container');
+    ayahContainer.innerText = '';
     ayahs.forEach(ayah => {
         console.log(ayah)
         const p = document.createElement('p');
@@ -48,5 +52,12 @@ const showAyah = ayahs => {
 }
 
 
-loadAyah()
-loadData()
+document.getElementById("surah-list").addEventListener('change', (event) => {
+    const surahNumber = event.target.value;
+    loadAyah(surahNumber);
+    // showAyah()
+    
+});
+
+loadAyah('1')
+loadSurah()
